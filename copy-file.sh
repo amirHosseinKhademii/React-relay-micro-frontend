32dbedacb4bed2d3b578843798e4cd8a3f264124
amirHosseinKhademii <sourena2575@gmail.com>
2023-06-22 16:56:28 +0200

    Add env variable

--- copy-file.sh
+++ copy-file.sh
@@ -1,12 +1,11 @@
+SOURCE_DIR=$1
 RG_NAME=AKH_auto_cd
 ACCOUNT_NAME=akhstorageauto
-
-ls
 ACCOUNT_KEY=$(az storage account keys list --account-name $ACCOUNT_NAME --resource-group $RG_NAME --query "[?keyName=='key1'].value" --output tsv)
 
 az storage blob upload-batch \
     --destination '$web' \
-    --source ./WebApp \
+    --source $SOURCE_DIR/WebApp \
     --account-name $ACCOUNT_NAME \
     --account-key $ACCOUNT_KEY \
     --overwrite
