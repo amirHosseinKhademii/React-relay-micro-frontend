2eb28970e85bccd5aadb1cd85a47d15fa49bc58b
amirHosseinKhademii <sourena2575@gmail.com>
2023-06-29 14:32:25 +0200

    Add public access

--- create-infra.sh
+++ create-infra.sh
@@ -1,6 +1,9 @@
   RG_NAME=AKH_auto_cd
   ACCOUNT_NAME=akhstorageauto
+  ACCOUNT_KEY=$(az storage account keys list --account-name $ACCOUNT_NAME --resource-group $RG_NAME --query "[?keyName=='key1'].value" --output tsv)
+
   
   az group create --location westeurope --name $RG_NAME
   az storage account create --name $ACCOUNT_NAME --resource-group $RG_NAME --sku Standard_LRS
   az storage blob service-properties update --account-name $ACCOUNT_NAME --static-website true
+  az storage container set-permission --name '$web' --account-name akhstorageauto --public-access container --account-key $ACCOUNT_KEY
