5c5f3a3b687cc2b69cb0df3bc3b208970a18ae54
amirHosseinKhademii <sourena2575@gmail.com>
2023-10-10 12:16:28 +0200

    Fix core issue

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -6,4 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), relay],
+  server: {
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001/graphql",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/graphql/, ""),
+      },
+    },
+  },
 });
